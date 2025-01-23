d71f6c9fe5f03a5fdbea18d7942863223c33e9a6
wilddog1979 <wilddog1979@users.noreply.github.com>
2025-01-23 23:18:52 +0100

    initial version (#1)
    * initial version
    
    * node_modules dir must not be pushed
    
    * node modules must not be ignored
    
    * node modules
    
    * testing simpler
    
    * extract version with default
    
    * syntax error
    
    * syntax error
    
    * missing require
    
    * value comparison
    
    * string
    
    * debug
    
    * tags must be fetched
    
    * cleanup debug
    
    * license file added
    
    * cleanup
    
    * aesthetics

---
+++ index.js
@@ -0,0 +1,18 @@
+const { exec } = require('child_process');
+const fs = require('fs');
+
+const tagPrefix = `${process.env.INPUT_PREFIX || ''}*`;
+
+exec(`git fetch --tags origin && git for-each-ref --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/${tagPrefix}"`, {cwd: null}, (err, tag, stderr) => {
+  tag = tag.trim();
+
+  if (err) {
+    console.log('Tag not found: ', stderr);
+    process.exit(1);
+  } else if (tag === "") {
+    tag = process.env.INPUT_DEFAULT
+  }
+
+  fs.appendFileSync(process.env.GITHUB_OUTPUT, `version=${tag}\n`);
+  process.exit(0);
+});
