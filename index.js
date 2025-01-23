d855abd611d222377c191c353dc18a6794dfb43a
wilddog1979 <ferenc.kemeny79@gmail.com>
2025-01-23 22:50:01 +0100

    extract version with default

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-const inputDefault = process.env.INPUT_DEFAULT
-console.log(`Input: ${inputDefault}`);
+const tagPrefix = `${process.env.INPUT_PREFIX || ''}*`;
+
+exec(`git for-each-ref --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/${tagPrefix}"`, {cwd: null}, (err, tag, stderr) => {
+  tag = tag.trim();
+
+  if (err) {
+    console.log('Tag not found: ', stderr);
+    process.exit(1);
+  } else if (tag == '') {
+    tag = process.env.INPUT_DEFAULT
+  }
+
+  fs.appendFileSync(process.env.GITHUB_OUTPUT, `version=${tag}\n`);
+  process.exit(0);
