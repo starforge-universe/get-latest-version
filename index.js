b3f3100d146f18ac9e23a5bc5351c2bc79c47c73
wilddog1979 <ferenc.kemeny79@gmail.com>
2025-01-23 23:11:21 +0100

    cleanup debug

--- index.js
+++ index.js
@@ -5,13 +5,11 @@ const tagPrefix = `${process.env.INPUT_PREFIX || ''}*`;
 
 exec(`git fetch --tags origin && git for-each-ref --sort=-creatordate --count 1 --format="%(refname:short)" "refs/tags/${tagPrefix}"`, {cwd: null}, (err, tag, stderr) => {
   tag = tag.trim();
-  console.log(`tag: ${tag}`)
 
   if (err) {
     console.log('Tag not found: ', stderr);
     process.exit(1);
   } else if (tag === "") {
-    console.log('Default version');
     tag = process.env.INPUT_DEFAULT
   }
 
